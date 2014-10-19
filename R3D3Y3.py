adad07ab24c404d14f27a538f98c17480100b6e5
William J. Appleton <wappleto@gmail.com>
2014-10-19 00:38:30 -0400

    R3D3Y3 Version 2.0

--- R3D3Y3.py
+++ R3D3Y3.py
@@ -1,6 +1,6 @@
 ##############################################################################################################
 #                       #   Parachat logging software AKA: "R3D3Y3"                                          #
-#  \                 /  #   Version 1.1.a                                                                    #
+#  \                 /  #   Version 2.0                                                                      #
 #   \      ( )      /   #   June 13, 2014                                                                    #
 #    \             /    #   Author: William J. Appleton (Mephistopheles)                                     #
 #     \           /     ######################################################################################
@@ -19,8 +19,20 @@
 #                       #   You should have received a copy of the GNU General Public License                #
 #                       #   along with this program.  If not, see <http://www.gnu.org/licenses/>.            #
 ##############################################################################################################
-import httplib ,urllib, time, re, datetime, json, sys, string, random
+import httplib ,urllib, time, re, datetime, json, random, bleach, unicodedata, codecs
 from random import randint
+from HTMLParser import HTMLParser
+def REPORTER(FileName, Message):
+	try:
+		Reporter = open(FileName, "a")
+		Reporter.write(unicode(datetime.datetime.now().date()) + u'~' + unicode(datetime.datetime.now().time()) + u'>>' + unicode(Message) + u'\n')
+		Reporter.close()
+	except Exception:
+		Reporter = open(FileName, "w+")
+		Reporter.close()
+		Reporter = open(FileName, "a")
+		Reporter.write(unicode(datetime.datetime.now().date()) + u'~' + unicode(datetime.datetime.now().time()) + u'>>' + unicode(Message) + u'\n')
+		Reporter.close()
 def USERNAME_GENERATOR(UserName_Length, UserName_ValidChars):
 	return ''.join(random.choice(UserName_ValidChars) for _ in range(UserName_Length))
 def LOGIN():
@@ -34,105 +46,51 @@ def LOGIN():
 			Login_Site_Reply = urllib.urlopen(Login_URL)
 			Login_Raw_Reply = Login_Site_Reply.read()
 			Chat_Key = str(re.search("key=(.*)&room=Lobby-2&user=" + UserName, re.search("<a href=\"(.*)\">Go to mobile chat room</a>",Login_Raw_Reply).group(1)).group(1))
-			try:
-				R3D3Y3Log = open('R3D3Y3.log', 'a')
-				R3D3Y3Log.write(str(datetime.datetime.now().date()) + "," + str(datetime.datetime.now().time()) + "," + "Initiated new session with username \"" + UserName + "\" and session key \"" + Chat_Key + "\"\n")
-				R3D3Y3Log.close()
-			except Exception:
-				R3D3Y3Log = open('R3D3Y3.log', 'w+')
-				R3D3Y3Log.close()
-				R3D3Y3Log = open('R3D3Y3.log', 'a')
-				R3D3Y3Log.write(str(datetime.datetime.now().date()) + "," + str(datetime.datetime.now().time()) + "," + "Initiated new session with username \"" + UserName + "\" and session key \"" + Chat_Key + "\"\n")
-				R3D3Y3Log.close()
+			REPORTER("R3D3Y3.log", "Initiated new session with username \"" + UserName + "\" and session key \"" + Chat_Key + "\"")
 			LoginFailed = 0
 			return str("http://members.parachat.com:8080/basic/wap/reqpub.jsp?site=iPhone&key=" + Chat_Key + "&room=iPhone&user=" + UserName)
 		except Exception:
-			try:
-				R3D3Y3Log = open('R3D3Y3.log', 'a')
-				R3D3Y3Log.write(str(datetime.datetime.now().date()) + "," + str(datetime.datetime.now().time()) + "," + "Failed to initiate a new session." + "\n")
-				R3D3Y3Log.close()
-				time.sleep(30)
-			except Exception:
-				R3D3Y3Log = open('R3D3Y3.log', 'w+')
-				R3D3Y3Log.close()
-				R3D3Y3Log = open('R3D3Y3.log', 'a')
-				R3D3Y3Log.write(str(datetime.datetime.now().date()) + "," + str(datetime.datetime.now().time()) + "," + "Failed to initiate a new session." + "\n")
-				R3D3Y3Log.close()
-				time.sleep(30)
-def MESSAGE_HANDLER(Messages):
-	Messages_Unformatted = re.search("(.*)</a>:(.*)",Messages)
-	try:
-		Messages_Divided = re.sub('" >', '\n', Messages_Unformatted.group())
-		Messages_ChatOnly = re.sub("(.*)javascript:paraShowOne(.*)", "\n", Messages_Divided) 
-		Messages_Formatted = str(re.sub("&amp;quot;", "\"", str(re.sub("&amp;amp;", "&", str(re.sub("&amp;lt;", "<", str(re.sub("&amp;gt;", ">", str(re.sub("<(.*?)>", "", str(re.sub('\n', '', str(re.sub('</a>: ',": ", str(re.sub("']", "", str(re.sub("', u'(.*)", "", Messages_ChatOnly)))))))))))))))))).decode('string_escape')
-		Messages_LogEntry = str(datetime.datetime.now().date()) + "~" + str(datetime.datetime.now().time()) + ">> " + str(Messages_Formatted) + "\n"
-		try:
-			ChatLogFile = open('ParachatLog.txt', 'a')
-			ChatLogFile.write(Messages_LogEntry)
-			ChatLogFile.close()
-		except Exception:
-			ChatLogFile = open('ParachatLog.txt', 'w+')
-			ChatLogFile.close()
-			ChatLogFile = open('ParachatLog.txt', 'a')
-			ChatLogFile.write(Messages_LogEntry)
-			ChatLogFile.close()
-	except Exception:
-		time.sleep(0)
+			REPORTER("R3D3Y3.log", "Failed to initiate a new session.")
+			time.sleep(120)
 def MAIN():
+	NoHTMLEscape = HTMLParser()
 	MessageURL = str(LOGIN())
-	MessageSuccess = 0 
+	MessageSuccess = 0
+	ProperMessage = re.compile(ur'(.*):(.*)', re.UNICODE)
+	PrivateMessage = re.compile(ur'^\[Private\]?', re.UNICODE)
 	while 1:
 		try:
 			Site_Reply = urllib.urlopen(MessageURL)
-			Raw_Messages = Site_Reply.read()
-			Messages = json.loads(Raw_Messages)
+			Messages = json.loads(Site_Reply.read())
 			if Messages['success']:
+				if MessageSuccess < 3:
+					REPORTER("R3D3Y3.log", "Successfully gathered new messages.")
+					MessageSuccess += 1
+				elif MessageSuccess == 3:
+					REPORTER("R3D3Y3.log", "...")
+					MessageSuccess += 1
 				try:
-					R3D3Y3Log = open('R3D3Y3.log', 'a')
-					if MessageSuccess < 3:
-						R3D3Y3Log.write(str(datetime.datetime.now().date()) + "," + str(datetime.datetime.now().time()) + "," + "Successfully gathered new messages." + "\n")
-						R3D3Y3Log.close()
-						MessageSuccess += 1
-					elif MessageSuccess == 3:
-						R3D3Y3Log.write(str(datetime.datetime.now().date()) + "," + str(datetime.datetime.now().time()) + "," + "..." + "\n")
-						R3D3Y3Log.close()
-						MessageSuccess += 1
+					for x in range(len(Messages['messages'])):
+						SingleMessage = NoHTMLEscape.unescape(bleach.clean(Messages['messages'][x].rstrip(), tags=[], strip=True)).strip()
+						if ProperMessage.match(SingleMessage) and not PrivateMessage.match(SingleMessage):
+							try:
+								StringMessage = str(SingleMessage)
+								if StringMessage != "":
+									with codecs.open("parachat.log", "a", encoding="utf-8") as ParachatLog:
+										LogMessage = re.sub(u'&amp;', u'&', re.sub(u'&gt;', u'>', re.sub(u'&lt;', u'<', re.sub(u'&quot;', u'"', SingleMessage, re.UNICODE), re.UNICODE), re.UNICODE), re.UNICODE)
+										ParachatLog.write(unicode(datetime.datetime.now().date()) + u'~' + unicode(datetime.datetime.now().time()) + u'>>' + LogMessage + u'\r\n')
+							except Exception:
+								with codecs.open("parachat.log", "a", encoding="utf-8") as ParachatLog:
+									LogMessage = re.sub(u'&amp;', u'&', re.sub(u'&gt;', u'>', re.sub(u'&lt;', u'<', re.sub(u'&quot;', u'"', SingleMessage, re.UNICODE), re.UNICODE), re.UNICODE), re.UNICODE)
+									ParachatLog.write(unicode(datetime.datetime.now().date()) + u'~' + unicode(datetime.datetime.now().time()) + u'>>' + LogMessage + u'\r\n')
 				except Exception:
-					R3D3Y3Log = open('R3D3Y3.log', 'w+')
-					R3D3Y3Log.close()
-					if MessageSuccess < 3:
-						R3D3Y3Log.write(str(datetime.datetime.now().date()) + "," + str(datetime.datetime.now().time()) + "," + "Successfully gathered new messages." + "\n")
-						R3D3Y3Log.close()
-						MessageSuccess += 1
-					elif MessageSuccess == 3:
-						R3D3Y3Log.write(str(datetime.datetime.now().date()) + "," + str(datetime.datetime.now().time()) + "," + "..." + "\n")
-						R3D3Y3Log.close()
-						MessageSuccess += 1
-				MESSAGE_HANDLER(str(Messages['messages']))
+					time.sleep(0)
 			else:
-				try:
-					R3D3Y3Log = open('R3D3Y3.log', 'a')
-					R3D3Y3Log.write(str(datetime.datetime.now().date()) + "," + str(datetime.datetime.now().time()) + "," + "Failed to gather new messages. Error returned: \"" + str(Messages['error']) + "\"\n")
-					R3D3Y3Log.close()
-				except Exception:
-					R3D3Y3Log = open('R3D3Y3.log', 'w+')
-					R3D3Y3Log.close()
-					R3D3Y3Log = open('R3D3Y3.log', 'a')
-					R3D3Y3Log.write(str(datetime.datetime.now().date()) + "," + str(datetime.datetime.now().time()) + "," + "Failed to gather new messages. Error returned: \"" + str(Messages['error']) + "\"\n")
-					R3D3Y3Log.close()
+				REPORTER("R3D3Y3.log", "Failed to gather new messages. Error {" + str(Messages['error']) + "}")
 				MessageSuccess = 0
 				MessageURL = str(LOGIN())
 		except Exception:
-			try:
-				R3D3Y3Log = open('R3D3Y3.log', 'a')
-				R3D3Y3Log.write(str(datetime.datetime.now().date()) + "," + str(datetime.datetime.now().time()) + "," + "Failied to connect to site or site returned unexpected data." + "\n")
-				R3D3Y3Log.close()
-			except Exception:
-				R3D3Y3Log = open('R3D3Y3.log', 'w+')
-				R3D3Y3Log.close()
-				R3D3Y3Log = open('R3D3Y3.log', 'a')
-				R3D3Y3Log.write(str(datetime.datetime.now().date()) + "," + str(datetime.datetime.now().time()) + "," + "Failied to connect to site or site returned unexpected data." + "\n")
-				R3D3Y3Log.close()
+			REPORTER("R3D3Y3.log", "Failied to connect to site or site returned unexpected data.")
 			MessageSuccess = 0
 			MessageURL = str(LOGIN())
 MAIN()
